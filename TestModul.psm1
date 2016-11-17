8d6fe9f9ed82aec3a4bfec37708e458d0332aae6
Martin Schreyer <martin.schreyer@noris.de>
2016-11-17 14:24:36 +0100

    initial commit

---
+++ TestModul.psm1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Kurzbeschreibung
+.DESCRIPTION
+   Lange Beschreibung
+.EXAMPLE
+   Beispiel für die Verwendung dieses Cmdlets
+.EXAMPLE
+   Ein weiteres Beispiel für die Verwendung dieses Cmdlets
+#>
+function Verb-Noun
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+
+        # Hilfebeschreibung zu Param2
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        "Temodul"
+    }
+    End
+    {
+    }
+}
+
+Export-ModuleMember -Function Verb-Noun
